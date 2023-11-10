e3b7cfea48f3822a3b2fe6a2e3f9478ee9048f7c
Wesley Millett <s539200@nwmissouri.edu>
2023-11-10 11:48:37 -0600

    Part 2 Complete

--- controllers/donut.js
+++ controllers/donut.js
@@ -1,14 +1,22 @@
 var Donut = require('../models/donut');
 
-exports.donut_list = function(req, res) {
+exports.donut_list = function (req, res) {
     res.send('NOT IMPLEMENTED: Donut list');
 };
 // for a specific Costume.
-exports.donut_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Donut detail: ' + req.params.id);
+exports.donut_detail = async function (req, res) {
+    try {
+        theDonuts = await Donut.find();
+        res.send(theDonuts);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+    //res.send('NOT IMPLEMENTED: Donut detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.donut_create_post = async function(req, res) {
+exports.donut_create_post = async function (req, res) {
     console.log(req.body)
     let document = new Donut();
     // We are looking for a body, since POST does not have query parameters.
@@ -18,31 +26,46 @@ exports.donut_create_post = async function(req, res) {
     document.donut_type = req.body.donut_type;
     document.num = req.body.num;
     document.price = req.body.price;
-    try{
+    try {
         let result = await document.save();
         res.send(result);
     }
-    catch(err){
+    catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
-    };
+};
 // Handle Costume delete form on DELETE.
-exports.donut_delete = function(req, res) {
+exports.donut_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Donut delete DELETE ' + req.params.id);
 };
 // Handle Costume update form on PUT.
-exports.donut_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: Donut update PUT' + req.params.id);
+exports.donut_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Donut.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.donut_type)
+            toUpdate.donut_type = req.body.donut_type;
+        if (req.body.num) toUpdate.num = req.body.num;
+        if (req.body.price) toUpdate.price = req.body.price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}failed`);
+    }
+    //res.send('NOT IMPLEMENTED: Donut update PUT' + req.params.id);
 };
 
 // List of all Costumes
-exports.donut_list = async function(req, res) {
-    try{
+exports.donut_list = async function (req, res) {
+    try {
         theDonuts = await Donut.find();
         res.send(theDonuts);
     }
-    catch(err){
+    catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
@@ -50,12 +73,12 @@ exports.donut_list = async function(req, res) {
 
 // VIEWS
 // Handle a show all view
-exports.donut_view_all_Page = async function(req, res) {
-    try{
+exports.donut_view_all_Page = async function (req, res) {
+    try {
         theDonuts = await Donut.find();
         res.render('donuts', { title: 'Donut Search Results', results: theDonuts });
     }
-    catch(err){
+    catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
