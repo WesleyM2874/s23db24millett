ac939fc18ad881d842eba271e557ba44ed4bb631
Wesley Millett <s539200@nwmissouri.edu>
2023-11-28 21:55:56 -0600

    login.pug, register.pug, and account.js Added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var Donut = require("./models/donut");
 
 require('dotenv').config();
 const connectionString =
-process.env.MONGO_CON
+  process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString);
 
@@ -10,41 +10,44 @@ mongoose.connect(connectionString);
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){
+db.once("open", function () {
   console.log("Connection to DB succeeded")
 });
 
 // Can seed the collection if needed on server start
-async function recreateDB(){
+async function recreateDB() {
   // Delete everything
   await Donut.deleteMany();
 
   let instance1 = new
-  Donut({donut_type:"glazed", num:"3", price:"4.0"});
+    Donut({ donut_type: "glazed", num: "3", price: "4.0" });
   let instance2 = new
-  Donut({donut_type:"frosted", num:"2", price:"3.5"});
+    Donut({ donut_type: "frosted", num: "2", price: "3.5" });
   let instance3 = new
-  Donut({donut_type:"cream", num:"1", price:"0.5"});
+    Donut({ donut_type: "cream", num: "1", price: "0.5" });
 
-  instance1.save().then(doc=>{
-    console.log("First object saved")}
-  ).catch(err=>{
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
     console.error(err)
   });
-  instance2.save().then(doc=>{
-    console.log("Second object saved")}
-  ).catch(err=>{
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
     console.error(err)
   });
-  instance3.save().then(doc=>{
-    console.log("Third object saved")}
-  ).catch(err=>{
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
     console.error(err)
   });
 }
 
 let reseed = true;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 
 var createError = require('http-errors');
 var express = require('express');
@@ -69,6 +72,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -78,12 +90,20 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.get('/gridbuild', function(req, res, next){
+app.get('/gridbuild', function (req, res, next) {
   let query = req.query;
   console.log('rows ${query.rows}');
   console.log('cols ${query.cols}');
@@ -92,7 +112,7 @@ app.get('/gridbuild', function(req, res, next){
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -106,22 +126,22 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-  Account.findOne({ username: username })
-  .then(function (user){
-  if (err) { return done(err); }
-  if (!user) {
-  return done(null, false, { message: 'Incorrect username.' });
-  }
-  if (!user.validPassword(password)) {
-  return done(null, false, { message: 'Incorrect password.' });
-  }
-  return done(null, user);
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
   })
-  .catch(function(err){
-  return done(err)
-  })
-  })
-  )
+)
 
 module.exports = app;
